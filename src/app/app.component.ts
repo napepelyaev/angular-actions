6955cc2a683fd230aaee497b953e139243a38a82
Николай Пепеляев <napepelyaev@gmail.com>
2023-04-11 21:12:49 +0500

    added documantation build and deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /**
+   * Заголовок стандартного компонента
+   */
   title = 'angular-actions';
 }

d387bddec59e86b8082ccd96e69ec9172f9f246b
Николай Пепеляев <napepelyaev@gmail.com>
2023-04-18 12:30:42 +0500

    Тест

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  /**
+   * Заголовок 2
+   */
   title = 'Auth0 Angular SDK Sample';
 
   constructor() {}
